5392cc0ae14edecc6674af200cba20d3467f42a7
Antoine Grand <antoine.grand@epitech.eu>
2023-11-26 10:25:03 +0100

    Shuffle the dayGifts.

--- scripts/daysGift.js
+++ scripts/daysGift.js
@@ -1,15 +1,7 @@
 let DaysGifts = [
     {
         type: 'text',
-        content: "<img src='./ressources/img/tasseCafe.jpeg'><p>Tasse Captain Tortue</p>"
-    },
-    {
-        type: 'text',
         content: "<img src='./ressources/img/SacZebre.jpeg'><p>Sac pochette zébré</p>"
-    },
-    {
-        type: 'text',
-        content: "<img src='./ressources/img/lunette.jpeg'><p>Lunettes de soleil</p>"
     },
     {
         type: 'text',
@@ -17,7 +9,7 @@ let DaysGifts = [
     },
     {
         type: 'text',
-        content: "<img src='./ressources/img/drapPlage.jpeg'><p>Drap de plage</p>"
+        content: "<img src='./ressources/img/pochetteRoseau.jpeg'><p>Pochette roseau</p>"
     },
     {
         type: 'text',
@@ -25,63 +17,15 @@ let DaysGifts = [
     },
     {
         type: 'text',
-        content: "<img src='./ressources/img/bonCadeau.jpeg'><p>Bon d'achat de 20€</p>"
-    },
-    {
-        type: 'text',
-        content: "<img src='./ressources/img/pochetteRoseau.jpeg'><p>Pochette roseau</p>"
-    },
-    {
-        type: 'text',
-        content: "<img src='./ressources/img/trousseCuir.jpeg'><p>Trousse cuir Paul Marius</p>"
-    },
-    {
-        type: 'text',
-        content: "<img src='./ressources/img/colier4rangs.jpeg'><p>Collier 4 rangs coloris gun</p>"
-    },
-    {
-        type: 'text',
-        content: "<img src='./ressources/img/sacJean.jpeg'><p>Sac en jean</p>"
-    },
-    {
-        type: 'text',
         content: "<img src='./ressources/img/bonCadeau.jpeg'><p>Bon d'achat de 45€</p>"
     },
     {
         type: 'text',
-        content: "<img src='./ressources/img/bonCadeau.jpeg'><p>Bon achat 15€</p>"
+        content: "<img src='./ressources/img/tasseCafe.jpeg'><p>Tasse Captain Tortue</p>"
     },
     {
         type: 'text',
-        content: "<img src='./ressources/img/gants.jpeg'><p>Paire de gant</p>"
-    },
-    {
-        type: 'text',
-        content: "<img src='./ressources/img/collection.jpg'><p>-30% sur ton article préféré collection printemps été 2024</p>"
-    },
-    {
-        type: 'text',
-        content: "<img src='./ressources/img/postIt.jpeg'><p>Ensemble post-it et stylo</p>"
-    },
-    {
-        type: 'text',
-        content: "<img src='./ressources/img/collection.jpg'><p>-50% sur ton accessoire préféré printemps été 2024</p>"
-    },
-    {
-        type: 'text',
-        content: "<img src='./ressources/img/echarpe.jpeg'><p>Echarpe</p>"
-    },
-    {
-        type: 'text',
-        content: "<img src='./ressources/img/gourde.jpeg'><p>Gourde Isotherme + Porte téléphone</p>"
-    },
-    {
-        type: 'text',
-        content: "<img src='./ressources/img/sacBandeSequin.jpeg'><p>Sac bande sequin</p>"
-    },
-    {
-        type: 'text',
-        content: "<img src='./ressources/img/poncho.jpeg'><p>Poncho intérieur</p>"
+        content: "<img src='./ressources/img/lunette.jpeg'><p>Lunettes de soleil</p>"
     },
     {
         type: 'text',
@@ -94,5 +38,61 @@ let DaysGifts = [
     {
         type: 'text',
         content: "<img src='./ressources/img/sacVoyage.jpeg'><p>Sac week-end</p>"
-    }
+    },
+    {
+        type: 'text',
+        content: "<img src='./ressources/img/drapPlage.jpeg'><p>Drap de plage</p>"
+    },
+    {
+        type: 'text',
+        content: "<img src='./ressources/img/trousseCuir.jpeg'><p>Trousse cuir Paul Marius</p>"
+    },
+    {
+        type: 'text',
+        content: "<img src='./ressources/img/collection.jpg'><p>-30% sur ton article préféré collection printemps été 2024</p>"
+    },
+    {
+        type: 'text',
+        content: "<img src='./ressources/img/sacJean.jpeg'><p>Sac en jean</p>"
+    },
+    {
+        type: 'text',
+        content: "<img src='./ressources/img/bonCadeau.jpeg'><p>Bon achat 15€</p>"
+    },
+    {
+        type: 'text',
+        content: "<img src='./ressources/img/gants.jpeg'><p>Paire de gant</p>"
+    },
+    {
+        type: 'text',
+        content: "<img src='./ressources/img/poncho.jpeg'><p>Poncho intérieur</p>"
+    },
+    {
+        type: 'text',
+        content: "<img src='./ressources/img/colier4rangs.jpeg'><p>Collier 4 rangs coloris gun</p>"
+    },
+    {
+        type: 'text',
+        content: "<img src='./ressources/img/postIt.jpeg'><p>Ensemble post-it et stylo</p>"
+    },
+    {
+        type: 'text',
+        content: "<img src='./ressources/img/collection.jpg'><p>-50% sur ton accessoire préféré printemps été 2024</p>"
+    },
+    {
+        type: 'text',
+        content: "<img src='./ressources/img/bonCadeau.jpeg'><p>Bon d'achat de 20€</p>"
+    },
+    {
+        type: 'text',
+        content: "<img src='./ressources/img/gourde.jpeg'><p>Gourde Isotherme + Porte téléphone</p>"
+    },
+    {
+        type: 'text',
+        content: "<img src='./ressources/img/sacBandeSequin.jpeg'><p>Sac bande sequin</p>"
+    },
+    {
+        type: 'text',
+        content: "<img src='./ressources/img/echarpe.jpeg'><p>Echarpe</p>"
+    },
 ]
